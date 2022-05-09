d3821d1be06d19faaaf1908c8874cc6017865574
Neibo Augustine <neiboaugustine@gmail.com>
2022-05-09 09:25:24 +0100

    building functionality for sell button and total price

---
+++ sales.js
@@ -0,0 +1,7 @@
+ let formSelect = document.querySelector('.form-select');
+ let salePrice = document.querySelector('.sale-price');
+ let sellButton = document.querySelector('.btn');
+
+ sellButton.addEventListener('click', ()=>{
+     salePrice.innerHTML = "Total price";
+ })
